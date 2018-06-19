89d9e73ff9a0dbd277184c5e3b87fa9bd23dfcf7
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-19 17:48:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -29,4 +29,12 @@ class School
     @roster
   end
   
+#  def sort
+#    sorted = {}
+#    roster.each do |grade, students|
+#      sorted[grade] = students.sort
+#    end
+#    sorted
+#  end
+  
 end
